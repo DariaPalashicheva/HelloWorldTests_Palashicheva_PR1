1936c7ba9180726e3240a54cd78f99908d148a4d
u31504 <u31504@P31504>
2022-11-19 09:24:01 +1000

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTests_Palashicheva_PR1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTests_Palashicheva_PR1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5107d14-b1c8-4d6f-89fd-56a581f55edb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace HelloWorldTests_Palashicheva_PR1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private const string Expected = "Hello World!";
+        private const string Expected2 = "Goodbye World!";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Palashicheva_PR1_testir.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Palashicheva_PR1_testir.Program.SayGoodbye();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected2, result);
+            }
+        }
+
+    }
+}
